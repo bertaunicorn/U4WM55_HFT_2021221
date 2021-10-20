9aa111044ffc10e7e9685ec120d3a2e374b84c2c
bertaunicorn <mach.berta@gmail.com>
2021-10-20 14:01:05 +0200

    created LooksRepository interface and class and added methods

---
+++ U4WM55_HFT_2021221.Repository/ILooksRepository.cs
@@ -0,0 +1,24 @@
+﻿using U4WM55_HFT_2021221.Models;
+
+namespace U4WM55_HFT_2021221.Repository
+{
+    /// <summary>
+    /// This is the interface for the looks repository.
+    /// </summary>
+    interface ILooksRepository : IRepository<Looks>
+    {
+        /// <summary>
+        /// Changes the theme of an existing look.
+        /// </summary>
+        /// <param name="id">Id of a look.</param>
+        /// <param name="newTheme">A string representing the new theme.</param>
+        void ChangeTheme(int id, string newTheme);
+
+        /// <summary>
+        /// Increases or decreases the difficulty of the look.
+        /// </summary>
+        /// <param name="id">Id of a look.</param>
+        /// <param name="newLookDiff">An int representing the new difficulty of the look.</param>
+        void ChangeLookDifficulty(int id, int newLookDiff);
+    }
+}
---
+++ U4WM55_HFT_2021221.Repository/LooksRepository.cs
@@ -0,0 +1,87 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Linq;
+using U4WM55_HFT_2021221.Models;
+
+namespace U4WM55_HFT_2021221.Repository
+{
+    /// <summary>
+    /// This is the LooksRepository class.
+    /// </summary>
+    class LooksRepository : Repository<Looks>, ILooksRepository
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="LooksRepository"/> class.
+        /// </summary>
+        /// <param name="context">We need a DbContext type parameter.</param>
+        public LooksRepository(DbContext context)
+            : base(context)
+        {
+        }
+
+        /// <summary>
+        /// Increases or decreases the difficulty of the look.
+        /// </summary>
+        /// <param name="id">Id of a look.</param>
+        /// <param name="newLookDiff">An int representing the new difficulty of the look.</param>
+        public void ChangeLookDifficulty(int id, int newLookDiff)
+        {
+            var look = this.GetOne(id);
+            if (look == null)
+            {
+                throw new InvalidOperationException("Look not found!");
+            }
+
+            look.Difficulty = newLookDiff;
+            this.Context.SaveChanges();
+        }
+
+        /// <summary>
+        /// Changes the theme of an existing look.
+        /// </summary>
+        /// <param name="id">Id of a look.</param>
+        /// <param name="newTheme">A string representing the new theme.</param>
+        public void ChangeTheme(int id, string newTheme)
+        {
+            var look = this.GetOne(id);
+            if (look == null)
+            {
+                throw new InvalidOperationException("Look not found!");
+            }
+
+            look.Theme = newTheme;
+            this.Context.SaveChanges();
+        }
+
+        /// <summary>
+        /// Finds one specific look based on the id.
+        /// </summary>
+        /// <param name="id">Id of a look.</param>
+        /// <returns>It returns a Looks object.</returns>
+        public override Looks GetOne(int id)
+        {
+            return this.GetAll().SingleOrDefault(x => x.Id == id);
+        }
+
+        /// <summary>
+        /// Adds a new Look.
+        /// </summary>
+        /// <param name="entity">A Looks type object.</param>
+        public override void Insert(Looks entity)
+        {
+            this.Context.Set<Looks>().Add(entity);
+            this.Context.SaveChanges();
+        }
+
+        /// <summary>
+        /// Deletes a Look.
+        /// </summary>
+        /// <param name="id">An integer representing the ID of a look.</param>
+        public override void Remove(int id)
+        {
+            Looks obj = this.GetOne(id);
+            this.Context.Set<Looks>().Remove(obj);
+            this.Context.SaveChanges();
+        }
+    }
+}
